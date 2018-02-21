608c5a2bea4f8227c1dd3c11fc443491814c312d
JaySoftye <JaySoftye@Creative-Department-Creative-2-iMac.local>
2018-02-21 09:41:26 -0500

    Update to contact form - animation slide

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -9,9 +9,61 @@
     <li><a href="#fourth-quarter-marker" alt="Consulting Services"><span>Consulting Services</span></a></li>
   </ul>
 </div>
+<script type="text/javascript">
+$(document).ready(function(){
+  $('div#contact button#open').on('click', function () {
+    $('div#contact div.contact-form').animate({right: "0", width: '120%', height: "120vh", bottom: "-10%" }, 250);
+    $('div.contact-form div#form').animate({width: '30%', opacity: '1'}, 250);
+  });
+  $('div#form button#close').on('click', function () {
+    $('div#contact div.contact-form').animate({right: "5%", width: '100px', height: "100px", bottom: "5.5%" }, 250);
+    $('div.contact-form div#form').animate({width: '0', opacity: '0'}, 250);
+  });
+});
+</script>
 <div id="contact">
-  <button class="btn" type="button" data-toggle="modal" data-target="#commentsModal"><img src="public/images/leave-comment-email-comments.svg" /></button>
+  <div class="contact-form">
+
+  <button id="open" class="btn">
+    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 5 5" >
+      <path style="fill:#FFFFFF;" d="M1.79,3.651L2.098,3.37l0.69,0.3l0.958-2.474L0.917,2.604l0.518,0.343L1.79,3.651z"/>
+      <polygon style="fill:#CAD3D4;" points="3.645,1.27 1.435,2.947 1.79,3.651 2.097,3.371 1.806,3.222 "/>
+      <polygon style="fill:#6C8588;" points="1.791,3.65 2.098,3.37 1.807,3.221 "/>
+    </svg>
+  </button>
+  <div id="form">
+    <button id="close" class="btn">
+      <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 100 100">
+      	<path style="fill:#FFFFFF;" d="M52.187,43.14c-0.224-0.225-0.434-0.462-0.859-0.916c0.335-0.214,0.655-0.347,0.883-0.573
+      		c2.852-2.833,5.7-5.672,8.532-8.525c1.713-1.726,1.741-3.971,0.061-5.733c-1.124-1.18-2.28-2.331-3.456-3.459
+      		c-1.793-1.717-4.025-1.685-5.79,0.069c-2.41,2.393-4.809,4.796-7.202,7.204c-0.65,0.654-1.262,1.345-1.891,2.018
+      		c-0.13,0.034-0.26,0.067-0.391,0.1c-0.177-0.276-0.311-0.594-0.537-0.822c-2.891-2.91-5.787-5.816-8.699-8.705
+      		c-1.729-1.715-4.014-1.713-5.719-0.023c-1.1,1.09-2.192,2.188-3.286,3.284c-1.927,1.929-1.922,4.121,0.022,6.068
+      		c2.86,2.865,5.724,5.724,8.585,8.588c0.222,0.222,0.428,0.46,0.651,0.702c-0.211,0.232-0.35,0.397-0.503,0.55
+      		c-2.919,2.921-5.847,5.831-8.755,8.764c-1.706,1.721-1.689,4.01,0.007,5.719c1.072,1.08,2.151,2.152,3.226,3.228
+      		c1.975,1.973,4.151,1.977,6.126,0.005c2.806-2.803,5.609-5.609,8.416-8.412c0.221-0.221,0.459-0.425,0.808-0.746
+      		c0.266,0.33,0.449,0.606,0.678,0.836c2.838,2.848,5.681,5.691,8.527,8.531c1.908,1.903,4.119,1.907,6.011,0.018
+      		c1.038-1.036,2.075-2.074,3.112-3.112c1.961-1.963,1.954-4.143-0.026-6.127C57.875,48.826,55.029,45.985,52.187,43.14z"/>
+      	<path style="fill:#FFFFFF;" d="M42.299,1.331c-22.69,0-41.084,18.394-41.084,41.084S19.608,83.5,42.299,83.5
+      		c22.691,0,41.085-18.394,41.085-41.084S64.989,1.331,42.299,1.331z M42.299,74.277c-17.568,0-31.861-14.293-31.861-31.861
+      		S24.73,10.554,42.299,10.554c17.569,0,31.861,14.293,31.861,31.862C74.16,59.985,59.868,74.277,42.299,74.277z"/>
+      </svg>
+    </button>
+    <h4 class="white">Send us your comments. If you have any questions or comments on any of the courses, products, or services we offer please send us your thoughts using the form below.</h4>
+<!--[if lte IE 8]>
+<script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2-legacy.js"></script>
+<![endif]-->
+      <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2.js"></script>
+      <script>
+        hbspt.forms.create({
+          portalId: '182596',
+          formId: '373eb659-2c8e-45e5-bada-86ef8c1c4e5c',
+        });
+      </script>
+  </div>
+  </div>
 </div>
+
 <div id="roadway">
   <img src="public/images/teq-roadmap-roadway.svg" />
 </div>
